b6cf19f66ab09cebb625ab9c6603a5b6116ea011
Ejim Michael <ejimmichael1@gmail.com>
2022-07-09 20:51:33 +0100

    Install heroicons

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -3,6 +3,7 @@ import React from "react";
 function SignIn() {
   return (
     <div className="shadow-lg flex items-center justify-center w-[90%] md:w-[50%] h-[90%] bg-white">
+
       <button>Sign In with Google</button>
     </div>
   );
