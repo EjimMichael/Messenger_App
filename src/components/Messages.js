df2219007f41c6235a612e98dbe10ddcdb4f373b
Ejim Michael <ejimmichael1@gmail.com>
2022-07-18 16:04:42 +0100

    Style form

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { IoSend } from "react-icons/io5";
 
 function Messages() {
   return (
-    <div>Messages</div>
+    <form className='flex mt-[50%]'>
+        <div className='ml-3'>
+            <input type="text" placeholder='Enter Message' className='w-[830px] h-8 rounded-full p-3' />
+        </div>
+        <div className=''>
+            <IoSend className='w-7 h-7 ml-2 '/>
+        </div>
+    </form>
   )
 }
 
