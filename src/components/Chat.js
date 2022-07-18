8abf9ba0f673fefd34a1d7e5067e7ae5cec70ae4
Ejim Michael <ejimmichael1@gmail.com>
2022-07-18 17:17:46 +0100

    Create Message component

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,14 +1,16 @@
 import { useEffect, useState } from 'react';
 import NavBar from './NavBar';
 import { db, auth } from "../firebaseConfig";
-import { collection, query, where, onSnapshot, addDoc, Timestamp } from "firebase/firestore";
+import { collection, query, where, onSnapshot, addDoc, Timestamp, orderBy } from "firebase/firestore";
 import User from './User';
 import Messages from './Messages';
+import Message from './Message';
 
 function Chat() {
   const [users, setUsers] = useState([]);
   const [chat, setChat] = useState('');
   const [text, setText] = useState('');
+  const [ msgs, setMsgs] = useState([]);
 
   const user1 = auth.currentUser.uid;
 
@@ -28,12 +30,27 @@ function Chat() {
   const selectUser = (user) => {
     setChat(user);
     console.log(user)
-  }
+
+    const user2 = user.uid;
+    const id = user1 > user2 ? `${user1 + user2}` : `${user2 + user1}`;
+    
+    const msgsRef = collection(db, 'messages', id, 'chat');
+    const q = query(msgsRef, orderBy('createdAt', 'asc'));
+    onSnapshot(q, querySnapshot => {
+      let msgs = [];
+      querySnapshot.forEach(doc => {
+        msgs.push(doc.data())
+      })
+      setMsgs(msgs);
+    }) 
+  };
+
+  console.log(msgs);
 
   const handleSubmit = async e => {
     e.preventDefault();
     const user2 = chat.uid;
-    const id = user1 > user2 ? `${user1 + user2}` : `${user2 + user1}`
+    const id = user1 > user2 ? `${user1 + user2}` : `${user2 + user1}`;
 
     await addDoc(collection(db, 'messages', id, 'chat'), {
       text,
@@ -59,6 +76,9 @@ function Chat() {
             <>
               <div className="border-b-2">
                 <h3 className="p-3 text-xl">{chat.name}</h3>
+              </div>
+              <div>
+                {msgs.length ? msgs.map((msg, index) => <Message key={index} msg={msg} />) : null}
               </div>
               <Messages handleSubmit={handleSubmit} text={text} setText={setText} />
             </>
--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React from "react";
 import { IoSend } from "react-icons/io5";
 
 function Messages({ handleSubmit, text, setText }) {
   return (
-    <form onSubmit={handleSubmit} className='flex mt-[50%]'>
-        <div className='ml-3'>
-            <input type="text" placeholder='Enter Message' value={text} onChange={e => setText(e.target.value)} className='w-[830px] h-8 rounded-full p-3' />
-        </div>
-        <div className=''>
-            <IoSend className='w-7 h-7 ml-2 '/>
-        </div>
+    <form onSubmit={handleSubmit} className="flex mt-[50%]">
+      <div className="ml-3">
+        <input
+          type="text"
+          placeholder="Enter Message"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          className="w-[830px] h-9 rounded-md p-3"
+        />
+      </div>
+      <div className="">
+        <IoSend className="w-8 h-8 ml-2 " />
+      </div>
     </form>
-  )
+  );
 }
 
 export default Messages;
