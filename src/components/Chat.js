5c4c268e26409f9ac20f136941f14a0a6eb122f3
Ejim Michael <ejimmichael1@gmail.com>
2022-07-18 15:02:07 +0100

    Messages component

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -3,6 +3,7 @@ import NavBar from './NavBar';
 import { db, auth } from "../firebaseConfig";
 import { collection, query, where, onSnapshot } from "firebase/firestore";
 import User from './User';
+import Messages from './Messages';
 
 function Chat() {
   const [users, setUsers] = useState([]);
@@ -34,10 +35,19 @@ function Chat() {
           ))}
         </div>
 
-        <div className='grid-cols-2 w-[200%]'>
-          {chat ? <div className=''>
-            <h3 className='p-3 text-xl border-b-2'>{chat.name}</h3>
-          </div> : <h3 className='text-2xl text-gray-600 text-center mt-[25%]'>Select a user to start a conversation</h3>}
+        <div className="grid-cols-2 w-[200%]">
+          {chat ? (
+            <>
+              <div className="border-b-2">
+                <h3 className="p-3 text-xl">{chat.name}</h3>
+              </div>
+              <Messages />
+            </>
+          ) : (
+            <h3 className="text-2xl text-gray-600 text-center mt-[25%]">
+              Select a user to start a conversation
+            </h3>
+          )}
         </div>
       </div>
     </div>
