2f24517929761f76603f6743360c66d8de338abc
Ejim Michael <ejimmichael1@gmail.com>
2022-07-18 16:32:20 +0100

    Text functionality

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { IoSend } from "react-icons/io5";
 
-function Messages() {
+function Messages({ handleSubmit, text, setText }) {
   return (
-    <form className='flex mt-[50%]'>
+    <form onSubmit={handleSubmit} className='flex mt-[50%]'>
         <div className='ml-3'>
-            <input type="text" placeholder='Enter Message' className='w-[830px] h-8 rounded-full p-3' />
+            <input type="text" placeholder='Enter Message' value={text} onChange={e => setText(e.target.value)} className='w-[830px] h-8 rounded-full p-3' />
         </div>
         <div className=''>
             <IoSend className='w-7 h-7 ml-2 '/>
